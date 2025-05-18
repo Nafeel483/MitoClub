370feee7a2d29fe2645e5102ca80bf9333640634
Nafeel483 <m.nafeelrasheed@gmail.com>
2025-05-18 07:39:14 +0500

    Add Ecosystem View

--- src/app/pages/LandingPage.tsx
+++ src/app/pages/LandingPage.tsx
@@ -1,7 +1,7 @@
 
 import Hero from "../components/Hero";
 import AboutUs from "../components/Aboutus";
-import EcoOverview from "../components/EcoOverview";
+import EcoOverview from "../components/Ecooverview";
 import Mah from "../components/Mah";
 import Logos from "../components/Logos";
 
