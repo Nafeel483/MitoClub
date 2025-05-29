280e1f50d8aca0ec73e343d64514d46de0c8868a
Nafeel483 <m.nafeelrasheed@gmail.com>
2025-05-29 14:23:27 +0500

    Fixed Mobile View Changes

--- src/app/components/Aboutus.tsx
+++ src/app/components/Aboutus.tsx
@@ -8,17 +8,15 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import localfont from "next/font/local";
 
-
-
 const UbuntuRegular = localfont({
   src: "../fonts/Ubuntu-Regular.ttf",
 });
-
+const UbuntuMedium = localfont({
+  src: "../fonts/Ubuntu-Medium.ttf",
+});
 const UbuntuMediumItalic = localfont({
   src: "../fonts/Ubuntu-MediumItalic.ttf",
 });
-
-
 
 const GradientCard = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -27,29 +25,28 @@ const GradientCard = ({ children }: { children: React.ReactNode }) => {
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
       transition={{ duration: 0.6 }}
-
-      className="relative rounded-4xl p-[2px] h-[405px] w-[308px] bg-[url('/images/card.png')] bg-contain bg-no-repeat">
-      <div className="rounded-4xl  bg-[#0B0620] p-[10px] text-center h-full flex flex-col justify-start">
+      className="relative rounded-4xl p-[2px] h-[405px] w-[308px] bg-[url('/images/card.png')] bg-contain bg-no-repeat"
+    >
+      <div className="rounded-4xl bg-[#0B0620] p-[10px] text-center h-full flex flex-col justify-start">
         {children}
       </div>
     </motion.div>
   );
 };
 
-
 export default function AboutUs() {
   return (
-    <section className=" py-16 px-6 bg-[#0B0620] mb-15">
+    <section className="py-16 px-6 bg-[#0B0620] mb-15">
       <div className="text-center mb-20">
-
         <h1
-          className={`font-[${UbuntuRegular.className}] text-[64px] font-bold leading-tight 
-              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] 
-              bg-clip-text text-transparent inline-block`}>
-          {`ABOUT US`}
+          className={`font-[${UbuntuRegular.className}] text-[40px] md:text-[64px] font-bold leading-tight 
+              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] bg-clip-text text-transparent inline-block`}>
+          ABOUT US
         </h1>
 
-        <p className={`max-w-xl mx-auto mt-5 text-[18] text-[#FFFFFF] font-normal font-[${UbuntuRegular.className}]`}>
+        <p
+          className={`max-w-xl mx-auto mt-5 text-[16px] md:text-[18px] text-[#FFFFFF] font-normal font-[${UbuntuRegular.className}]`}
+        >
           MITO is a next-generation Decentralized Finance AI (DeFAI) protocol
           designed combining intelligent trading, real-world asset integration,
           and decentralized insurance to form sustainable and scalable
@@ -62,27 +59,29 @@ export default function AboutUs() {
         </p>
       </div>
 
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 w-[94%] self-center place-self-center"> */}
-      <section className="relative w-[94%] self-center place-self-center flex flex-row  gap-6 items-center justify-center">
+      <section
+        className="relative w-full max-w-7xl mx-auto grid gap-6 justify-center 
+          grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
         <GradientCard>
           <Image
             src={one}
             alt="Leo Tan"
             width={200}
             height={200}
-            className="object-cover rounded-b-2xl mx-auto mb-1 mt-2" />
-
-          <h3 className={`font-[${UbuntuRegular.className}] text-[24px] font-medium text-center leading-tight 
-              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] 
-              bg-clip-text text-transparent inline-block`}>
-            {`Leo Tan`}
-          </h3>
-
-          <p className={`text-[#5F00FF] text-[20] text-center mb-2 italic font-[${UbuntuMediumItalic.className}]`}>Founder</p>
-          <p className={`text-[19] text-[#FFFFFF] text-center mb-4 font-[${UbuntuRegular.className}]`}>
-            Based in Malaysia | Blockchain Visionary | Crypto Investor | Fintech Pioneer.
+            className="object-cover rounded-b-2xl mx-auto mb-2 mt-2"
+          />
+          <h1 className={`font-[${UbuntuMedium.className}] text-[24px] text-center leading-tight bg-[linear-gradient(90deg,_#9795FF_30%,_#FFFFFF_42%,_#FFFFFF_69%,_#BE9FFF_100%)] bg-clip-text text-transparent inline-block`}>
+            Leo Tan
+          </h1>
+          <p
+            className={`text-[#5F00FF] text-[20] text-center mb-2 italic font-[${UbuntuMediumItalic.className}]`}>
+            Founder
           </p>
-
+          <p
+            className={`text-[19] text-[#FFFFFF] text-center mb-4 font-[${UbuntuRegular.className}]`}>
+            Based in Malaysia | Blockchain Visionary | Crypto Investor |
+            Fintech Pioneer.
+          </p>
         </GradientCard>
 
         <GradientCard>
@@ -91,20 +90,21 @@ export default function AboutUs() {
             alt="K Krypto"
             width={200}
             height={200}
-            className="object-cover rounded-b-2xl mx-auto mb-1 mt-2" />
-
-          <h3 className={`font-[${UbuntuRegular.className}] text-[24px] font-medium text-center leading-tight 
-              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] 
-              bg-clip-text text-transparent inline-block`}>
-            {`K Krypto`}
-          </h3>
-
-          <p className={`text-[#5F00FF] text-[20] text-center  mb-2 italic font-[${UbuntuMediumItalic.className}]`}>{'Chief Marketing Officer (CMO)'}</p>
-          <p className={`text-[19] text-[#FFFFFF] text-center mb-4 font-[${UbuntuRegular.className}]`}>
-            {`Based in China Mainland | DEFAI Strategist | AD-driven Marketing Expert | NFT & Crypto Growth Specialist.`}
+            className="object-cover rounded-b-2xl mx-auto mb-2 mt-2"
+          />
+          <h1 className={`font-[${UbuntuMedium.className}] text-[24px] text-center leading-tight bg-[linear-gradient(90deg,_#9795FF_30%,_#FFFFFF_42%,_#FFFFFF_69%,_#BE9FFF_100%)] bg-clip-text text-transparent inline-block`}>
+            K Krypto
+          </h1>
+          <p
+            className={`text-[#5F00FF] text-[20] text-center mb-2 italic font-[${UbuntuMediumItalic.className}]`}>
+            Chief Marketing Officer (CMO)
+          </p>
+          <p
+            className={`text-[19] text-[#FFFFFF] text-center mb-4 font-[${UbuntuRegular.className}]`}>
+            Based in China Mainland | DEFAI Strategist | AD-driven Marketing
+            Expert | NFT & Crypto Growth Specialist.
           </p>
         </GradientCard>
-
 
         <GradientCard>
           <Image
@@ -112,17 +112,20 @@ export default function AboutUs() {
             alt="Renaenys"
             width={200}
             height={200}
-            className="object-cover rounded-b-2xl mx-auto mb-1 mt-2" />
+            className="object-cover rounded-b-2xl mx-auto mb-2 mt-2"
+          />
 
-          <h3 className={`font-[${UbuntuRegular.className}] text-[24px] font-medium text-center leading-tight 
-              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] 
-              bg-clip-text text-transparent inline-block`}>
-            {`Renaenys`}
-          </h3>
-
-          <p className={`text-[#5F00FF] text-[20] text-center  mb-2 italic font-[${UbuntuMediumItalic.className}]`}>{'Chief Technology Officer (CTO)'}</p>
-          <p className={`text-[19] text-[#FFFFFF] text-center  mb-4  font-[${UbuntuRegular.className}]`}>
-            {`Based in Iceland | Blockchain Architect | AD-driven Trading Specialist | DEFAI Innovator.`}
+          <h1 className={`font-[${UbuntuMedium.className}] text-[24px] text-center leading-tight bg-[linear-gradient(90deg,_#9795FF_30%,_#FFFFFF_42%,_#FFFFFF_69%,_#BE9FFF_100%)] bg-clip-text text-transparent inline-block`}>
+            Renaenys
+          </h1>
+          <p
+            className={`text-[#5F00FF] text-[20] text-center mb-2 italic font-[${UbuntuMediumItalic.className}]`} >
+            Chief Technology Officer (CTO)
+          </p>
+          <p
+            className={`text-[19] text-[#FFFFFF] text-center mb-4 font-[${UbuntuRegular.className}]`}>
+            Based in Iceland | Blockchain Architect | AD-driven Trading
+            Specialist | DEFAI Innovator.
           </p>
         </GradientCard>
 
@@ -132,22 +135,21 @@ export default function AboutUs() {
             alt="Mito Admin"
             width={200}
             height={200}
-            className="object-cover rounded-b-2xl mx-auto mb-1 mt-2" />
-
-          <h3 className={`font-[${UbuntuRegular.className}] text-[24px] font-medium text-center leading-tight 
-              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] 
-              bg-clip-text text-transparent inline-block`}>
-            {`Mito Admin`}
-          </h3>
-
-          <p className={`text-[#5F00FF] text-[20] text-center mb-2 italic font-[${UbuntuMediumItalic.className}]`}>{'Head of Community & Operations'}</p>
-          <p className={`text-[19] text-[#FFFFFF] text-center  mb-4  font-[${UbuntuRegular.className}]`}>
-            {`Based in Malaysia | Community Leader.`}
+            className="object-cover rounded-b-2xl mx-auto mb-2 mt-2"
+          />
+          <h1 className={`font-[${UbuntuMedium.className}] text-[24px] text-center leading-tight bg-[linear-gradient(90deg,_#9795FF_30%,_#FFFFFF_42%,_#FFFFFF_69%,_#BE9FFF_100%)] bg-clip-text text-transparent inline-block`}>
+            Mito Admin
+          </h1>
+          <p
+            className={`text-[#5F00FF] text-[20] text-center mb-2 italic font-[${UbuntuMediumItalic.className}]`}>
+            Head of Community & Operations
           </p>
-
+          <p
+            className={`text-[19] text-[#FFFFFF] text-center mb-4 font-[${UbuntuRegular.className}]`}>
+            Based in Malaysia | Community Leader.
+          </p>
         </GradientCard>
       </section>
-      {/* </div> */}
     </section>
   );
 }
--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -1,139 +1,94 @@
-
 "use client";
 
 import trophy from '../../../public/images/trophy.png';
-import rocket from '../../../public/images/rocket.png'
-import person from '../../../public/images/person.png'
+import rocket from '../../../public/images/rocket.png';
+import person from '../../../public/images/person.png';
 import Image from 'next/image';
 import localfont from "next/font/local";
 
-
-const UbuntuRegular = localfont({
-  src: "../fonts/Ubuntu-Regular.ttf",
-});
-
-const UbuntuMedium = localfont({
-  src: "../fonts/Ubuntu-Medium.ttf",
-});
-const Ubuntubold = localfont({
-  src: "../fonts/Ubuntu-Bold.ttf",
-});
-
-const UbuntuItalic = localfont({
-  src: "../fonts/Ubuntu-Italic.ttf",
-});
+const UbuntuRegular = localfont({ src: "../fonts/Ubuntu-Regular.ttf" });
+const UbuntuMedium = localfont({ src: "../fonts/Ubuntu-Medium.ttf" });
+const Ubuntubold = localfont({ src: "../fonts/Ubuntu-Bold.ttf" });
+const UbuntuItalic = localfont({ src: "../fonts/Ubuntu-Italic.ttf" });
 
 const Cards = () => {
   return (
-    <section className="relative w-[94%] self-center place-self-center flex flex-col items-center justify-center py-25">
-      <div className="w-full flex flex-wrap gap-6 justify-center">
+    <section className="relative w-[94%] self-center flex flex-col items-center justify-center py-16">
+      <div className="w-full flex flex-col lg:flex-row flex-wrap gap-6 justify-center items-center">
 
-        {/* 1 */}
-        <div className="w-[30%] h-[423px] bg-white/5 rounded-3xl border-[1px] border-solid border-white/25 items-end">
-          <div className={`mt-20 text-center`} >
-
-
-            <p className={`font-[${Ubuntubold.className}] text-[26px] font-bold
-              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] 
-              bg-clip-text text-transparent inline-block text-center`}>
-              {"Bybit TOP ROI Master Trader"}
+        {/* Card 1 */}
+        <div className="w-full sm:w-[80%] md:w-[48%] lg:w-[30%] h-[423px] bg-white/5 rounded-3xl border border-white/25 flex flex-col justify-between p-4">
+          <div className="mt-6 text-center">
+            <p className={`text-[22px] md:text-[26px] font-bold bg-gradient-to-r from-[#9795FF] via-white to-[#BE9FFF] bg-clip-text text-transparent ${Ubuntubold.className}`}>
+              Bybit TOP ROI Master Trader
             </p>
-            <p className={`font-[${Ubuntubold.className}] text-[#FFFFFF] text-[48px] font-bold text-center`} >
-              {"$ 3631,318,833"}
+            <p className={`text-white text-[36px] md:text-[48px] font-bold ${Ubuntubold.className}`}>
+              $ 3631,318,833
             </p>
           </div>
-
-          <div className="flex flex-row justify-between mt-15">
-            <div className={`ml-10`}>
-              <p className={`text-[#FFFFFF] font-medium text-[20px] font-[${UbuntuMedium.className}]`}>{"Total Trading Volume "} </p>
-              <p className={`text-[#FFFFFF] font-normal text-[20px] font-[${UbuntuRegular.className}]`}>{"(2017-2025) "} </p>
-              <p className={`text-[#8740FF] text-[15px] font-normal mt-1 italic font-[${UbuntuItalic.className}]`} >
-                {"Click for info"}
+          <div className="flex flex-row justify-between items-end mt-4">
+            <div className="ml-2 md:ml-6">
+              <p className={`text-white text-[18px] md:text-[20px] font-medium ${UbuntuMedium.className}`}>Total Trading Volume</p>
+              <p className={`text-white text-[16px] md:text-[20px] ${UbuntuRegular.className}`}> (2017-2025)</p>
+              <p className={`text-[#8740FF] text-[14px] md:text-[15px] italic mt-1 ${UbuntuItalic.className}`}>
+                Click for info
               </p>
             </div>
-
-            <Image
-              src={trophy}
-              alt="Trophy"
-              width={210}
-              height={300}
-             style={{ objectFit: "contain" }}
-            />
+            <Image src={trophy} alt="Trophy" width={140} height={180} className="object-contain" />
           </div>
         </div>
 
-
-        {/* 2 */}
-        <div className="w-[30%] h-[423px] bg-white/5 rounded-3xl border-[1px] border-solid border-white/25 items-end">
-          <div className={`mt-20 text-center`} >
-            <p className={`font-[${Ubuntubold.className}] text-[26px] font-bold
-              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] 
-              bg-clip-text text-transparent inline-block text-center`}>
-              {"Leading the DEFAI Revolution"}
+        {/* Card 2 */}
+        <div className="w-full sm:w-[80%] md:w-[48%] lg:w-[30%] h-[423px] bg-white/5 rounded-3xl border border-white/25 flex flex-col justify-between p-4">
+          <div className="mt-6 text-center">
+            <p className={`text-[22px] md:text-[26px] font-bold bg-gradient-to-r from-[#9795FF] via-white to-[#BE9FFF] bg-clip-text text-transparent ${Ubuntubold.className}`}>
+              Leading the DEFAI Revolution
             </p>
-
           </div>
-          <div className={`ml-5 flex flex-row items-center`}>
-            <p className={`font-[${Ubuntubold.className}] text-[#FFFFFF] text-[50px] font-bold`} >
-              {"8"}
+          <div className="ml-2 md:ml-5 flex flex-row items-center mt-2">
+            <p className={`text-white text-[36px] md:text-[50px] font-bold ${Ubuntubold.className}`}>
+              8
             </p>
-            <div className={`ml-2`}>
-              <p className={`font-[${UbuntuRegular.className}] text-[#FFFFFF] text-[17px] font-normal`}>{"years of experience"}</p>
-              <p className={`font-[${UbuntuRegular.className}] text-[#FFFFFF] text-[17px] font-normal`}>{"in the crypto industry"}</p>
+            <div className="ml-2">
+              <p className={`text-white text-[15px] md:text-[17px] ${UbuntuRegular.className}`}>years of experience</p>
+              <p className={`text-white text-[15px] md:text-[17px] ${UbuntuRegular.className}`}>in the crypto industry</p>
             </div>
-
           </div>
-
-          <div className="flex flex-row justify-between items-center mt-10">
-            <div className={`ml-10 -mt-6`}>
-              <p className={`text-[#FFFFFF] font-normal text-[15px] font-[${UbuntuRegular.className}]`}>{`MITO founded by Leo, 
-integrates AI-driven trading, automated staking,and intelligent liquidity solutions—shapingthe future of decentralized finance.`} </p>
+          <div className="flex flex-row justify-between items-center mt-4">
+            <div className="ml-2 md:ml-6 -mt-4">
+              <p className={`text-white text-[14px] md:text-[15px] ${UbuntuRegular.className}`}>
+                MITO founded by Leo, integrates AI-driven trading, automated staking, and intelligent liquidity solutions—shaping the future of decentralized finance.
+              </p>
             </div>
-
-            <Image
-              src={rocket}
-              alt="Rocket"
-              width={210}
-              height={222}
-              style={{ objectFit: "contain" }}
-            />
+            <Image src={rocket} alt="Rocket" width={140} height={160} className="object-contain" />
           </div>
         </div>
 
-        {/* 3 */}
-        <div className="w-[30%] h-[423px] bg-white/5 rounded-3xl border-[1px] border-solid border-white/25 items-end">
-          <div className={`mt-20 ml-10`} >
-            <p className={`font-[${Ubuntubold.className}] text-[26px] font-bold
-              bg-[linear-gradient(90deg,_#9795FF_0%,_#FFFFFF_42%,_#FFFFFF_59%,_#BE9FFF_100%)] 
-              bg-clip-text text-transparent inline-block text-center`}>
-              {"Registered Users"}
+        {/* Card 3 */}
+        <div className="w-full sm:w-[80%] md:w-[48%] lg:w-[30%] h-[423px] bg-white/5 rounded-3xl border border-white/25 flex flex-col justify-between p-4">
+          <div className="mt-6 ml-2 md:ml-6">
+            <p className={`text-[22px] md:text-[26px] font-bold bg-gradient-to-r from-[#9795FF] via-white to-[#BE9FFF] bg-clip-text text-transparent ${Ubuntubold.className}`}>
+              Registered Users
             </p>
           </div>
-          <div className={`ml-10 flex flex-row items-end`}>
-            <p className={`font-[${Ubuntubold.className}] text-[#FFFFFF] text-[48px] font-bold text-center`} >{"51,281"}</p>
-            <p className={`text-[#FFFFFF] font-medium text-[16px] pb-3 font-[${UbuntuMedium.className}]`}>{"and growing rapidly!"} </p>
+          <div className="ml-2 md:ml-6 flex flex-row items-end">
+            <p className={`text-white text-[36px] md:text-[48px] font-bold ${Ubuntubold.className}`}>51,281</p>
+            <p className={`text-white text-[14px] md:text-[16px] pb-2 md:pb-3 ${UbuntuMedium.className}`}>and growing rapidly!</p>
           </div>
-
           <div className="flex flex-row justify-between items-center">
-            <div className={`ml-10`}>
-              <p className={`text-[#FFFFFF] font-medium text-[20px] font-[${UbuntuMedium.className}]`}>{"Global Presence: "} </p>
-              <p className={`text-[#FFFFFF] font-normal text-[13px] font-[${UbuntuRegular.className}]`}>{"Indonesia, Taiwan, Hong Kong, China Mainland, Singapore, Malaysia."} </p>
+            <div className="ml-2 md:ml-6">
+              <p className={`text-white text-[18px] md:text-[20px] font-medium ${UbuntuMedium.className}`}>Global Presence:</p>
+              <p className={`text-white text-[12px] md:text-[13px] ${UbuntuRegular.className}`}>
+                Indonesia, Taiwan, Hong Kong, China Mainland, Singapore, Malaysia.
+              </p>
             </div>
-
-            <Image
-              src={person}
-              alt="Person"
-              width={200}
-              height={222}
-              style={{ objectFit: "contain" }}
-            />
+            <Image src={person} alt="Person" width={140} height={160} className="object-contain" />
           </div>
         </div>
-
 
       </div>
     </section>
-  )
+  );
 }
 
 export default Cards;
